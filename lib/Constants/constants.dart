e33d30a2beff510d904d28c34b674c1d5f234b88
wampet <wampet232@gmail.com>
2022-03-08 16:02:22 +0300

    first commit

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,3 @@
+ import 'package:flutter/material.dart';
+const mainColor= Color(0xFFF0777C);
+const scaffoldBackgroundColor= Color(0xFFFFF8FC);
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+Future <void> main() async {
+
+
+  runApp(MaterialApp(
+    theme: ThemeData.light().copyWith(
+    primaryColor: const Color(0xFFF0777C),
+        scaffoldBackgroundColor: const Color(0xFFFFF8FC),
+    ),
+        // initialRoute: '/splashScreen',
+        routes: {
+          
+        },
+      ));
+}
+
+
+
+
