4bb9658d7df6c2dcc82d7f0d5602b83472412f9b
dorothyas <dorotheeasiimwe@gmail.com>
2022-04-06 11:27:10 +0300

    Add FAQs changes

--- lib/Screens/Faqs.dart
+++ lib/Screens/Faqs.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 import 'package:breast_health_app/Constants/constants.dart';
 import 'package:breast_health_app/Widgets/accordion.dart';
-import 'package:breast_health_app/models/models.dart';
+import 'package:breast_health_app/Models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,16 +23,17 @@ class _FaqsAccordionState extends State<FaqsAccordion> {
 
   getFaqs() async {
     final response = await http.get(Uri.parse(faqsUrl));
+
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
       List data = responseData['faqs'];
       var faqs = data.forEach((faq) {
-        final Faqs faqs = Faqs(question: faq['faq_question'], answer: faq['faq_answer']);
+        final Faqs faqs =
+            Faqs(question: faq['faq_question'], answer: faq['faq_answer']);
         setState(() {
-        _faqs.add(faqs);
+          _faqs.add(faqs);
+        });
       });
-      });
-
     } else {
       throw Exception('Failed to fetch data');
     }
@@ -41,33 +42,29 @@ class _FaqsAccordionState extends State<FaqsAccordion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: mainColor,
-          centerTitle: true,
-          title: const Text(
-            'Frequent questions',
-          ),
+      appBar: AppBar(
+        backgroundColor: mainColor,
+        centerTitle: true,
+        title: const Text(
+          'Frequent questions',
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child:Column(children: [
-          ListView.builder(
-          shrinkWrap: true,
-          itemCount: _faqs.length,
-          itemBuilder: (BuildContext context, int index) {
-            var faq = _faqs[index];
-            return Accordion(
-                title: faq.question,
-                content:faq.answer
-                );
-          },
-        )
-
-        ]),
-              ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: _faqs.length,
+              itemBuilder: (BuildContext context, int index) {
+                var faq = _faqs[index];
+                return Accordion(title: faq.question, content: faq.answer);
+              },
+            )
+          ]),
         ),
-        );
+      ),
+    );
   }
 }
 
